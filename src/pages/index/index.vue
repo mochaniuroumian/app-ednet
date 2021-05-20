c976187d7a8abc258de97cc345fa1153983ef787
mochaniuroumian <763372894@qq.com>
2021-05-20 17:41:14 +0800

    页面跳转

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -18,6 +18,7 @@
 			                      <image mode="aspectFit" src="/static/Group 25.png"></image>
 			                      <view><text>微信小程序</text><text>注册.开发.代维</text></view>
 		                        </view>
+								<button type="primary" @tap="call('4006511688')">免费升级</button>
 							</view>
                         </swiper-item>
                         <swiper-item>
@@ -29,8 +30,13 @@
                     </swiper>
                 </view>
             </view>
-			<uni-icons class="leftright arrowleft" type="arrowleft" size="50" color="#f8f8f888" @click.stop="leftImg"></uni-icons>
-            <uni-icons class="leftright arrowright" type="arrowright" size="50" color="#f8f8f888" @click.stop="rightImg"></uni-icons>
+			<uni-icons class="leftright arrowleft" type="arrowleft" size="50" color="#c7c7c799" @click.stop="leftImg"></uni-icons>
+            <uni-icons class="leftright arrowright" type="arrowright" size="50" color="#c7c7c799" @click.stop="rightImg"></uni-icons>
+		</view>
+		<view class="index-next">
+		    <button open-type="getUserInfo" hover-class="none" plain="true" @getuserinfo="nextpage">
+			  <image mode="aspectFit" src="/static/you.png"></image>
+		    </button>
 		</view>
 	</view>
 	<image class="background" mode="aspectFill" src="/static/background.jpg"></image>
@@ -42,7 +48,9 @@ import {uniIcons} from '@dcloudio/uni-ui'
 	export default {
 		data() {
 			return {
-				curDot: 0
+				curDot: 0,
+				username: '',
+				userimg: ''
 			}
 		},
 		onLoad() {
@@ -67,7 +75,26 @@ import {uniIcons} from '@dcloudio/uni-ui'
 	        } else {
 		      this.curDot++
 	        }
-          }
+          },
+		  call(phone){
+            uni.makePhoneCall({phoneNumber: phone})
+		  },
+		  nextpage() {
+            //this.userdetail = res.detail; 
+			uni.getUserInfo({
+              success: res => {
+                this.username = res.userInfo.nickName
+				this.userimg = res.userInfo.avatarUrl
+				uni.redirectTo({
+					url: '/pages/product/product?name=' + this.username + '&img=' + this.userimg,
+				})
+              },
+			  fail() {
+				this.username = '未登录用户'
+				this.userimg = '/static/logo.png'
+			  }
+			});
+		  }
 		},
 		components: {uniIcons}
 	}
---
+++ src/pages/product/product.vue
@@ -0,0 +1,23 @@
+<template>
+    <view class="container product">
+      <view class="hello">
+          <view class="user">欢迎您，{{uname}}</view>
+          <view class="user-img"><image mode="aspectFit" :src="uimg"></image></view>
+      </view>
+      <image class="background" mode="aspectFill" src="/static/background.jpg"></image>
+    </view>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            uname: '',
+			uimg: ''
+        }
+    },
+    onLoad(options) {
+        this.uname = options.name
+        this.uimg = options.img
+    },
+}
+</script>
